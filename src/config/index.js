d34588c4cc5fb4215e1805e453ab1af429d06fae
cainiaodjm <cainiaodjm@163.com>
2018-12-07 23:49:21 +0800

    vue.config.js update

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 //设置baseUrl路径 如果是生成环境就为http//production.com  否则就为测试环境下的服务端url 
 //如果要设置代理的话 要为'' 并且在vue.config.js中设置代理地址
 export const baseURL=process.env.NODE_ENV==='production'?
-'http//production.com':'http://localhost:4000'
+'http//production.com':''
 // export const baseURL=process.env.NODE_ENV==='production'?
 // 'http//production.com':''
 
